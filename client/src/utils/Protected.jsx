5e6109410b982621142f8154f42af2ffdebda8c8
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-10-11 04:10:33 +0530

    svg add, minor bug fix

---
+++ client/src/utils/Protected.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function Protected({ children }) {
+    const navigate = useNavigate();
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate("/");
+        }
+    }, [navigate, userInfo]);
+
+    return children;
+}
+
+export default Protected;
