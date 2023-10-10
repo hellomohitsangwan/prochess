917e8053c0bf6d5e36a3d40be994e2c0414d68b3
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-10-11 02:33:43 +0530

    redux store and reducers mapping

---
+++ client/src/store.js
@@ -0,0 +1,34 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailReducer,
+} from "./reducers/userReducer";
+
+const reducer = combineReducers({
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailReducer
+});
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+const initialState = {
+  userLogin: {
+    userInfo: userInfoFromStorage, 
+    loading: false
+  },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
