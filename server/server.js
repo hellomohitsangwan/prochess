04f939657e4fe5ef8b8a9bdb5e95f7dad78df238
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-10-13 15:11:53 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
+import corsMiddleware from "./cors/index.js"; 
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 app.use(express.json());
 connectDB();
+const server = http.createServer(app);
 
 app.use("/api/users", userRoutes);
+app.use(corsMiddleware);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected: ", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
 
 const PORT = 8000;
 app.listen(PORT, () => {
