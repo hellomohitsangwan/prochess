919bfacf8776ece53af75f73ee6e0635911a8190
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-10-13 23:01:11 +0530

    UI of online gme selection screen

--- client/src/screens/SelectGameScreen/SelectOnline.jsx
+++ client/src/screens/SelectGameScreen/SelectOnline.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import dropdown from "../../assets/dropdown.svg";
-import io from "socket.io-client"
-
-
+import io from "socket.io-client";
+import ps from "../../assets/ps.png";
+import "./Index.css";
+import Header from "../../components/Header";
 
 function SelectOnline() {
   const navigate = useNavigate();
@@ -19,53 +20,60 @@ function SelectOnline() {
     if (keyword === "") {
       setIsDisabled(true);
     } else {
-      setIsDisabled(false);
+      setIsDisabled(false );
     }
   }, [keyword]);
 
   return (
     <>
-      <div className="container">
-        <div className="info">
-          <button className="info-button">
-            Create New Game
-            <img src={dropdown} alt="" />
-          </button>
-          <ul>
-            <li>
-              <Link to="/play/online">Black</Link>
-            </li>
-            <li>
-              <li>
-                <Link to="/play/online">White</Link>
-              </li>
-            </li>
-          </ul>
+    <Header />
+      <div className="maincont">
+        <div className="left">
+          <img style={{width: "24rem"}} src={ps} alt="img" />
         </div>
 
-        <h2 style={{ marginRight: "32px" }}>OR</h2>
-
-        <form onSubmit={submitHandler}>
-          <div className="input-group">
-            <input
-              type="text"
-              id="search_field"
-              className="form-control"
-              placeholder="Enter Game ID ...."
-              onChange={(e) => setKeyword(e.target.value)}
-            />
-            <div className="input-group-append">
-              <button
-                id="search_btn"
-                className="btn"
-                onClick={submitHandler}
-                disabled={isdisabled}
-              >
-                Play
-              </button>
-            </div>
+        <div className="right">
+          <div className="info">
+            <button className="info-button">
+              Create New Game
+              <img style={{marginLeft: "8px"}} src={dropdown} alt="" />
+            </button>
+            <ul>
+              <li>
+                <Link to="/play/online">Black</Link>
+              </li>
+              <li>
+                <li>
+                  <Link to="/play/online">White</Link>
+                </li>
+              </li>
+            </ul>
           </div>
-        </form>
+
+          <h2 style={{ marginRight: "32px" }}>OR</h2>
+
+          <form onSubmit={submitHandler}>
+            <div className="input-group">
+              <input
+                type="text"
+                id="search_field"
+                className="form-control"
+                placeholder="Enter Game ID ...."
+                onChange={(e) => setKeyword(e.target.value)}
+              />
+              <div className="input-group-append">
+                <button
+                  id="search_btn"
+                  className="b4"
+                  onClick={submitHandler}
+                  disabled={isdisabled}
+                >
+                  Play
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
